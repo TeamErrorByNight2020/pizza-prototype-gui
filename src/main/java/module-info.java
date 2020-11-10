40521b9e1e6f8a27aac5771f395e9811c58aebf1
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2020-11-10 15:52:26 -0500

    Create Maven archetype project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module io.github.teamerrorbynight2020 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens io.github.teamerrorbynight2020 to javafx.fxml;
+    exports io.github.teamerrorbynight2020;
+}
